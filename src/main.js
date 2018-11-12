85bb38de7fb53600ddc138c5712c0c58d76585fa
eclero <eclero357@gmail.com>
2018-11-12 17:51:51 +0900

    エラーが出ていたのでマテリアルアイコンをaddした

--- src/main.js
+++ src/main.js
@@ -8,7 +8,9 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'vuetify/dist/vuetify.min.css'
 import './registerServiceWorker'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  iconfont: 'mdi' // 'md' || 'mdi' || 'fa' || 'fa4'
+})
 
 Vue.config.productionTip = false
 
