3fab8adf3f973a88a4f06f07fc3c21bb4cc090ed
eclero <eclero357@gmail.com>
2018-11-13 14:24:24 +0900

    フッターを作った

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
     <div class="content-area">
       <router-view/>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from '@/components/Footer.vue'
 import Header from '@/components/Header.vue'
 
 export default {
   name: 'app',
   components: {
     Header,
+    Footer
   }
 }
 </script>
@@ -25,8 +28,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  /* background-color: #DCEDC8; */
   color: #2c3e50;
 }
-
 /* 明るめの緑 #42b983 */
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="footer">
+    <v-bottom-nav :value="true" color="light-green darken-2" dark app>
+      <v-btn color="white" to="/" flat>
+        <span>ホーム</span>
+        <v-icon>home</v-icon>
+      </v-btn>
+
+      <v-btn color="white" flat>
+        <span>エリア</span>
+        <v-icon>place</v-icon>
+      </v-btn>
+
+      <v-btn color="white" flat>
+        <span>クリア済み</span>
+        <v-icon>done_outline</v-icon>
+      </v-btn>
+
+      <v-btn color="white" flat>
+        <span>使い方</span>
+        <v-icon>help</v-icon>
+      </v-btn>
+    </v-bottom-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
