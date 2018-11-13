3e85d3c92aff67d3f1ddf3bbb873168d5303b2ba
eclero <eclero357@gmail.com>
2018-11-13 13:57:50 +0900

    ヘッダーを作った

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue      from 'vue'
+import App      from './App.vue'
 import firebase from 'firebase/app';
 import router   from './router'
 import store    from './store'
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
+import Vue    from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Quiz from './views/Quiz.vue'
+import Home   from './views/Home.vue'
+import Quiz   from './views/Quiz.vue'
 import Result from './views/Result.vue'
 
 Vue.use(Router)
@@ -15,16 +15,17 @@ export default new Router({
       name: 'home',
       component: Home,
     },
-    {
-      path: '/quiz/1',
-      name: 'quiz',
-      component: Quiz,
-    },
-    {
-      path: '/result',
-      name: 'result',
-      component: Result,
-    },
+    // {
+      // path: '/quiz/:id',
+    //   path: 'quiz/HOGE01FUGA23PIYO45',
+    //   name: 'quiz',
+    //   component: Quiz,
+    // },
+    // {
+    //   path: '/result',
+    //   name: 'result',
+    //   component: Result,
+    // },
     // {
     //   path: '/hoge',
     //   name: 'hoge',
