a41e059e2c4f45c6e4a21a3c83486f35cd1e3b76
eclero <eclero357@gmail.com>
2018-12-11 16:26:20 +0900

    storeni認証のロジックを実装

--- src/store.js
+++ src/store.js
@@ -6,15 +6,45 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    quizList: quizList
+    isLoggedIn: false,
+    quizList: quizList,
+    user: {}
   },
+
   getters: {
-    quizList: (state) => state.quizList
+    quizList: (state) => state.quizList,
+    user:     (state) => state.user
   },
+
   mutations: {
-
+    setUser(state, { userData }) {
+      state.user = userData;
+      state.isLoggedIn = true;
+    },
+    clearUser(state) {
+      state.user = null;
+      state.isLoggedIn = false;
+    }
   },
-  actions: {
 
+  actions: {
+    logIn({ commit }) {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then((result) => {
+        const user = result.user;
+        const userData = {
+          id:       user.uid,
+          email:    user.email,
+          name:     user.name,
+          photoUrl: user.photoURL,
+        }
+        commit('setUser', { userData });
+      });
+    },
+    logOut({ commit }) {
+      firebase.auth().signOut().then(() => {
+        commit('clearUser');
+      });
+    }
   }
 })
