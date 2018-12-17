b9b871aadad8d5148526ec0ea8e63bbab99f94dc
eclero <eclero357@gmail.com>
2018-12-17 22:41:00 +0900

    クリア済みページにカードを追加

---
+++ src/components/ResultCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card color="white">
+    <v-card-title primary-title>
+      <div>Q{{ quiz.number }}</div>
+      <span>{{ quiz.title }}</span>
+    </v-card-title>
+    <v-card-actions>
+      <v-btn icon @click="show = !show">
+        <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+    <v-card-text v-show="show">
+      説明文
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import quizList from '@/quizList.json';
+
+export default {
+  name: 'ResultCard',
+
+  props: {
+    quizId: {
+      type:     String,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      show: false
+    }
+  },
+
+  computed: {
+    quiz() {
+      return quizList[this.$props.quizId]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-card__title span {
+  text-align: left;
+}
+
+.v-card {
+  margin-bottom: 15px;
+}
+</style>
+
--- src/views/ClearList.vue
+++ src/views/ClearList.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="clear-list">
+  <div class="clear-list pa-3">
+    <ResultCard quizId="1a8c40e105e93ac6856a" />
+    <ResultCard quizId="f1ad65d9d29c9d8b154d" />
   </div>
 </template>
 
 <script>
+import ResultCard from '@/components/ResultCard'
+
 export default {
   name: 'ClearList',
+  components: {
+    ResultCard
+  }
 }
 </script>
 
