2da94932e92d9ac4ce4c989df42cc6cc396434bc
eclero <eclero357@gmail.com>
2018-12-17 22:45:00 +0900

    カードを7枚に増やしてスタイルを整えた

--- src/views/ClearList.vue
+++ src/views/ClearList.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="clear-list pa-3">
+  <div class="clear-list pa-4">
     <ResultCard quizId="1a8c40e105e93ac6856a" />
     <ResultCard quizId="f1ad65d9d29c9d8b154d" />
+    <ResultCard quizId="7fe9b47cc6c0836222af" />
+    <ResultCard quizId="cb5f850a9811d2a4bc3a" />
+    <ResultCard quizId="c70105f684e6816621d2" />
+    <ResultCard quizId="ac4c19dd36cd182841bc" />
+    <ResultCard quizId="db8495fac3fbc6cd2569" />
   </div>
 </template>
 
@@ -15,7 +20,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-
-</style>
