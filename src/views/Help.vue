1a11d633a060d857420fb9d96574d526d5cc1bcc
eclero <eclero357@gmail.com>
2018-12-19 11:43:45 +0900

    helpページに説明を追加

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="help">
+    <v-card color="white">
+      <v-card-text>
+        <p>1. 右下のカメラボタンを押してQRカメラを起動</p>
+        <p>2. QRを読み取って「クイズへ」ボタンをタップ</p>
+        <p>3. 4択クイズに答えることでヒントを入手できます</p>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
@@ -10,5 +17,13 @@ export default {
 </script>
 
 <style scoped>
+.help {
+  text-align: left;
+  margin: 20px;
+  margin-top: 90px;
+}
 
+h3 {
+  margin-bottom: 20px;
+}
 </style>
