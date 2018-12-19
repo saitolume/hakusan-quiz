bb04692b1b9040476b1c72e6edfa89681521e55b
eclero <eclero357@gmail.com>
2018-12-19 11:56:43 +0900

    homeページに説明を追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="home">
+    <h2>
+      <p>はくさんクイズへようこそ</p>
+      <p>このアプリでは謎解きのヒントを得ることができます</p>
+    </h2>
   </div>
 </template>
 
@@ -10,5 +14,14 @@ export default {
 </script>
 
 <style scoped>
-
+h2 {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  width: 80%;
+  height: 25%;
+}
 </style>
